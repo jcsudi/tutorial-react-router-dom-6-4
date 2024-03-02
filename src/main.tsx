a2681d1507c6d252d0f24f5ac149c81fc4f25277
José Carlos de Souza <jcjrta@gmail.com>
2024-03-02 17:14:15 -0300

    No match route

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App.tsx'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Expenses from './routes/Expenses/index.tsx'
 import Invoices from './routes/Invoices/index.tsx'
+import NotFound from './routes/NotFpund/index.tsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
@@ -13,6 +14,9 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         <Route path="/" element={<App />} >
           <Route path="expenses" element={<Expenses />} />
           <Route path="invoices" element={<Invoices />} />
+          <Route
+             path="*" element={<NotFound />}
+             />
         </Route>
       </Routes>
   </BrowserRouter>,
